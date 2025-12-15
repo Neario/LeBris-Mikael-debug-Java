2450bd4595bab81a845ba46a7e12e0a6ab7c3648
Mika.el <lebris.mika@gmail.com>
2025-12-15 13:41:55 +0100

    remove unnecessary comments

---
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -0,0 +1,46 @@
+package com.hemebiotech.analytics;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class AnalyticsCounter {
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
+
+    /**
+     *
+     * @param args
+     * @throws Exception
+     * Lance un programme afin de lire dans le fichier symptoms ,
+     * ligne par ligne afin de compter le nombre de symptome identique et de sortir dans un fichier result.out
+     */
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		String line = reader.readLine();
+
+        while (line != null) {
+            System.out.println("symptom from file: " + line);
+			if (line.equals("headache")) {
+                headacheCount++;
+				System.out.println("number of headaches: " + headacheCount);
+			}
+			else if (line.equals("rash")) {
+				rashCount++;
+			}
+			else if (line.equals("dialated pupils")) {
+				pupilCount++;
+			}
+			line = reader.readLine();
+		}
+		
+		// next generate output
+		FileWriter writer = new FileWriter ("result.out");
+		writer.write("headache: " + headacheCount + "\n");
+		writer.write("rash: " + rashCount + "\n");
+		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.close();
+	}
+}
