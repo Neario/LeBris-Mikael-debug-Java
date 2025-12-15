829b446bb8471fe9dd13abb0bfa65df211d70fee
Mika.el <lebris.mika@gmail.com>
2025-12-15 22:15:43 +0100

    refactor analytics counter

--- src/AnalyticsCounter.java
+++ src/AnalyticsCounter.java
@@ -3,10 +3,19 @@ import com.hemebiotech.analytics.data.interfaces.ISymptomReader;
 import com.hemebiotech.analytics.repository.WriteSymptomDataToFile;
 import com.hemebiotech.analytics.repository.interfaces.ISymptomWriter;
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class AnalyticsCounter {
+
+    private final ISymptomReader symptomReader;
+    private ISymptomWriter symptomWriter;
+
+    public AnalyticsCounter(ISymptomReader symptomReader, ISymptomWriter symptomWriter) {
+        this.symptomReader = symptomReader;
+        this.symptomWriter = symptomWriter;
+    }
 
     /**
      *
@@ -17,14 +26,31 @@ public class AnalyticsCounter {
      */
 	public static void main(String[] args) throws Exception {
 
-        ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
+        ISymptomReader symptomReader= new ReadSymptomDataFromFile("symptoms.txt");
         Map<String, Integer> symptoms = new TreeMap<>();
         ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
 
-        for (String symptom : reader.getSymptoms()) {
+        for (String symptom : symptomReader.getSymptoms()) {
             int number = symptoms.getOrDefault(symptom, 0) + 1;
             symptoms.put(symptom, number);
         }
         writer.symptomsWriterFile(symptoms);
 	}
+
+    public List<String> getSymptoms() {
+        return symptomReader.getSymptoms();
+    }
+
+    public Map<String, Integer> countSymptoms(List<String> symptoms) {
+        Map<String, Integer> countSymptoms = new TreeMap<>();
+        for (String symptom : symptoms) {
+            int number = countSymptoms.getOrDefault(symptom, 0) + 1;
+            countSymptoms.put(symptom, number);
+        }
+        return countSymptoms;
+    }
+
+    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+        return symptoms;
+    }
 }
